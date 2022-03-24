985eb869d2c85550dcf8ce50e0b5d426ba65939a
Dan Carmichael <benevolentwhale@gmail.com>
2022-03-23 20:05:00 -0500

    complete

--- demo/src/main/java/com/example/demo/api/PersonController.java
+++ demo/src/main/java/com/example/demo/api/PersonController.java
@@ -2,6 +2,8 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +15,7 @@ public class PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
@@ -39,7 +42,7 @@ public class PersonController {
     }
 
     @PutMapping(path = "{id}")
-    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate) {
+    public void updatePerson(@PathVariable("id") UUID id, @NonNull @RequestBody Person personToUpdate) {
         personService.updatePerson(id, personToUpdate);
     }
 
---
+++ demo/src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,59 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
+        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person =  jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+            UUID personID = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(personId, name);
+        });
+        return Optional.ofNullable(person);
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
